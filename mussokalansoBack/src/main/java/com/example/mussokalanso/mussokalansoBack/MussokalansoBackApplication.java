b31793d685306c418ee60fc32b9aea3a24c370da
Alassane SANOGO <alassanesanogojumeau@gmail.com>
2022-01-31 13:50:30 +0000

    initialization

---
+++ mussokalansoBack/src/main/java/com/example/mussokalanso/mussokalansoBack/MussokalansoBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mussokalanso.mussokalansoBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MussokalansoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MussokalansoBackApplication.class, args);
+	}
+
+}
---
+++ mussokalansoBack/src/test/java/com/example/mussokalanso/mussokalansoBack/MussokalansoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mussokalanso.mussokalansoBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MussokalansoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
