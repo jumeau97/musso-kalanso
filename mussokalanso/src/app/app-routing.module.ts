7536deed13215ceb2d11dd51422197c5d0065f79
Alassane SANOGO <alassanesanogojumeau@gmail.com>
2022-02-02 12:41:36 +0000

    UI initialize

--- mussokalanso/src/app/app-routing.module.ts
+++ mussokalanso/src/app/app-routing.module.ts
@@ -2,10 +2,48 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  },
+  {
+    path: 'tabs/items/:id',
+    loadChildren: () => import('./pages/tabs/item/item.module').then( m => m.ItemPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'video/details',
+    loadChildren: () => import('./pages/tabs/video-details/video-details.module').then( m => m.VideoDetailsPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/auth/signup/signup.module').then( m => m.SignupPageModule)
   }
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
+  // {
+  //   path: 'auth',
+  //   loadChildren: () => import('./pages/auth/auth.module').then( m => m.AuthPageModule)
+  // },
+  // {
+  //   path: 'tabs',
+  //   loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)
+  // }
 ];
 @NgModule({
   imports: [
