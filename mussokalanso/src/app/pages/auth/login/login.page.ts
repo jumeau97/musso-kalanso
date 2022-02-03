5638001e7fdba38e8aac689cb166405d49ed0838
Alassane SANOGO <alassanesanogojumeau@gmail.com>
2022-02-03 13:57:57 +0000

    login began

--- mussokalanso/src/app/pages/auth/login/login.page.ts
+++ mussokalanso/src/app/pages/auth/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
+loginForm = new FormGroup({
+  email: new FormControl('', [Validators.required, Validators.email]),
+  motDePasse: new FormControl('', [Validators.required])
+});
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.warn(this.loginForm.value);
   }
 
 }
