25077d04be015de98848bc90d9ae060855986823
Awa KEITA <awakeita627@gmail.com>
2022-03-11 10:01:46 +0000

    Read-module

--- mussokalanso/src/app/admin/read-module/read-module.page.ts
+++ mussokalanso/src/app/admin/read-module/read-module.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-read-module',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadModulePage implements OnInit {
   item: any;
-  constructor() { }
+  constructor(public navCtrl: NavController,) { }
 
   ngOnInit() {
   }
