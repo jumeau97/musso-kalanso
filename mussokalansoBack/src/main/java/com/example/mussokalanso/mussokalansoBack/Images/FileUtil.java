9860ad3427a9d75115158a023655cfb9c3709e04
Alassane SANOGO <alassanesanogojumeau@gmail.com>
2022-03-15 17:11:56 +0000

    upload and form

---
+++ mussokalansoBack/src/main/java/com/example/mussokalanso/mussokalansoBack/Images/FileUtil.java
@@ -0,0 +1,13 @@
+package com.example.mussokalanso.mussokalansoBack.Images;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public final class FileUtil {
+    private FileUtil() {
+        // restrict instantiation
+    }
+
+    public static final String folderPath =  "incoming-files//";
+    public static final Path filePath = Paths.get(folderPath);
+}
