4864bf83298d097f16573336484f03aa3ae7fa36
Awa KEITA <awakeita627@gmail.com>
2022-04-01 10:56:36 +0000

    UpdateReadModule

--- mussokalansoBack/src/main/java/com/example/mussokalanso/mussokalansoBack/Image/FileUtil.java
+++ mussokalansoBack/src/main/java/com/example/mussokalanso/mussokalansoBack/Image/FileUtil.java
@@ -8,6 +8,6 @@ public final class FileUtil {
         // restrict instantiation
     }
 
-    public static final String folderPath =  "C:\\Users\\alassane.sanogo\\mussokalanso\\images\\";
+    public static final String folderPath =  "C:\\Users\\awa.keita\\mussokalanso\\images\\";
     public static final Path filePath = Paths.get(folderPath);
 }
